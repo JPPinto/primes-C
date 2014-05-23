9e52a991b911adf0da01ccdb2708b7d207aeadb1
JPPinto <jose.vc.pinto@gmail.com>
2014-05-23 11:21:20 +0100

    Adding thread functions, incomplete.

--- primes.c
+++ primes.c
@@ -4,20 +4,39 @@
 #include <math.h>
 #include "circularQueue.h"
 
+#define QUEUE_CAPACITY 5
+
 int * primes;
+int currentIndex = 0;
 
-int main(int argc, char * argv[], char * envp[]){
+int main(int argc, char * argv[], char * envp[]) {
 
-	if(argc < 2){
-		fprintf(stderr, "Usage: %s primeNumber\n", argv[0]);
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s primeNumber\n", argv[0]);
         exit(1);
-	}
+    }
 
-	int amountOfNumbers = atoi(argv[1]);
+    int amountOfNumbers = atoi(argv[1]);
+    int numberOfPrimes = 1.2 * ((double) amountOfNumbers / log((double) amountOfNumbers)); //1.2*(N / ln N)
 
-	int numberOfPrimes = 1.2 * ((double)amountOfNumbers / log((double)amountOfNumbers)); //1.2*(N / ln N)
+    primes = (int *) malloc(sizeof (int) * numberOfPrimes);
 
-	primes = (int *)malloc(sizeof(int) * numberOfPrimes); 
+    exit(EXIT_SUCCESS);
+}
 
-	exit(EXIT_SUCCESS);
+void initialThread(void * numMax) {
+    int x = (int) numMax;
+
+    primes[currentIndex++] = 2;
+
+    if (numMax > 2) {
+        CircularQueue * q;
+        queue_init(&q, 5);
+        
+        
+    }
+}
+
+void filteringThread(){
+
 }
